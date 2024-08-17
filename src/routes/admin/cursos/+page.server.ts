65c1ba0abce664d68953300ab08564640545b58c
Diego Astorga <diegoastorga04@gmail.com>
2024-08-17 10:49:02 -0500

    cursos ui

--- src/routes/admin/cursos/+page.server.ts
+++ src/routes/admin/cursos/+page.server.ts
@@ -1,9 +1,9 @@
 export async function load({ locals, url }) {
 	let page = parseInt(url.searchParams.get('page') || "1")
 	
-	const resultList = await locals.pb.collection('students').getList(page, 3, {
+	const resultList = await locals.pb.collection('courses').getList(1, 1, {
 		sort: 'name',
-		// expand: 'user_id,career'
+		expand: 'career'
 	});
 
 	return {
